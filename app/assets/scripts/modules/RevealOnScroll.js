4bec590bd2e20b7f8052c360ded53825013d1fa2
Al <al@systemsaholic.com>
2023-05-27 21:25:12 -0400

    Added the Reveal On Scroll Feature

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,58 @@
+import throttle from "lodash/throttle"
+import debounce from "lodash/debounce"
+
+class RevealOnScroll {
+  constructor(els, thresholdPercent) {
+    this.itemsToReveal = els
+    this.thresholdPercent = thresholdPercent
+    this.browserHeight = window.innerHeight
+    this.hideInitially()
+    this.scrollThrottle = throttle(this.calcCaller, 200).bind(this)
+    this.events()
+  }
+
+  events() {
+    window.addEventListener("scroll", this.scrollThrottle)
+    window.addEventListener(
+      "resize",
+      debounce(() => {
+        console.log("resize ran")
+        this.browserHeight = window.innerHeight
+      }, 333)
+    )
+  }
+
+  calcCaller() {
+    console.log("Scroll Ran")
+    this.itemsToReveal.forEach(el => {
+      const isRevealed = el.dataset.revealed === "true"
+      if (!isRevealed) {
+        this.calculateIfScrolledTo(el)
+      }
+    })
+  }
+
+  calculateIfScrolledTo(el) {
+    if (window.scrollY + this.browserHeight > el.offsetTop) {
+      console.log("Element was Calculated")
+      const scrollPercent = (el.getBoundingClientRect().top / this.browserHeight) * 100
+      if (scrollPercent < this.thresholdPercent) {
+        el.classList.add("reveal-item--is-visible")
+        el.dataset.revealed = "true"
+        if (el.isLastItem) {
+          window.removeEventListener("scroll", this.scrollThrottle)
+        }
+      }
+    }
+  }
+
+  hideInitially() {
+    this.itemsToReveal.forEach(el => {
+      el.classList.add("reveal-item")
+      el.dataset.revealed = "false"
+    })
+    this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true
+  }
+}
+
+export default RevealOnScroll

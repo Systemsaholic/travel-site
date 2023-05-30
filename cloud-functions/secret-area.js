e88e7e733a0ac7385bbb34a7ccfbdebcde24cf72
Al <al@systemsaholic.com>
2023-05-30 17:33:59 -0400

    added proper response to server function

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,25 @@
 exports.handler = (event, context, callback) => {
-  callback(null, {
-    statusCode: 200,
-    body: "Welcome to the Secret Area"
-  })
+  const secretContent = `
+  <h3>Welcome to Secret Area</h3>
+  <p>Here we can tell you the sky is blue, and two plus two is eaual to 4</p>
+  `
+
+  let body
+
+  if (event.body) {
+    body = JSON.parse(even.body)
+  } else {
+    body = {}
+  }
+
+  if (body.password == "javascript") {
+    callback(null, {
+      statusCode: 200,
+      body: secretContent
+    })
+  } else {
+    callback(null, {
+      statusCode: 401
+    })
+  }
 }
